de250b9eb92ac350a65edcbf459c8d2b6c114ed6
Yurii Dilai <dilayyura@gmail.com>
2023-08-18 01:28:22 +0300

    crud practice

--- src/route/index.js
+++ src/route/index.js
@@ -1,9 +1,64 @@
 // Підключаємо технологію express для back-end сервера
+const e = require('express')
 const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
 
 // ================================================================
+
+class User {
+  static #list = []
+
+  constructor (email, login, password) {
+    this.email = email
+    this.login = login
+    this.password = password
+    this.id = new Date().getTime()
+  }
+
+  verifyPassword = (password) => this.password === password
+
+  static add = (user) => {
+    this.#list.push(user)
+  }
+
+  static getList = () => {
+    return this.#list
+  }
+
+  static getById = (id) => this.#list.findIndex((user) => user.id === id)
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex((user) => user.id === id)
+
+    if(index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static updateById = (id, data) => {
+    const user = this.getById(id);
+
+    if(user) {
+      this.update(user, data)
+      return true
+    } else {
+      return false
+    }
+  } 
+
+  static update = (user, { email }) => {
+    if (email) {
+      user.email = email
+    }
+  }
+}
+
+// ================================================================
+
 
 // router.get Створює нам один ентпоїнт
 
@@ -11,15 +66,91 @@ const router = express.Router()
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
+  const list = User.getList();
+
   // ↙️ cюди вводимо назву файлу з сontainer
   res.render('index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
     style: 'index',
+
+    data: {
+      users: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
   })
   // ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
+
+router.post('/user-create', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  const {email, login, password} = req.body;
+
+
+  const user = new User(email, login, password);
+
+  User.add(user)
+
+  console.log(User.getList())
+
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('success-info', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'success-info',
+    info: 'Користувач створений',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.get('/user-delete', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  const { id } = req.query;
+
+  User.deleteById(Number(id))
+
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('success-info', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'success-info',
+    info: 'Користувача видалено',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.post('/user-update', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  const { email, password, id } = req.body;
+
+  let result = false;
+
+  const user = User.getById(Number(id))
+
+  if (user.verifyPassword(password)) {
+    User.update(user, {email})  
+    result = true;
+  }
+
+  
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('success-info', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'success-info',
+    info: result ? 'Дані успішно змінено' : 'Виникла помилка',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
