2ec43f09037b44d8adcea35606e369e75fee1105
Yurii Dilai <dilayyura@gmail.com>
2023-08-20 18:08:40 +0300

    upd

--- src/route/index.js
+++ src/route/index.js
@@ -4,6 +4,23 @@ const express = require('express')
 const router = express.Router()
 
 class Product {
+  static #list = []
+
+  
+
+  constructor(name, price, description) {
+    this.id = this.generateId();
+    this.name = name;
+    this.price = price;
+    this.description = description
+    this.createDate = new Date().toISOString;
+  }
+
+  static generateId = () => {
+    const randomId = Math.floor(Math.random() * 90000) + 10000;
+    return randomId;
+  }
+
   
 }
 
